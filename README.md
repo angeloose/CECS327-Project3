0a5d4c6aaf703227b58f3ace5cd8227e269b156a
ea269 <emmanuelandrade189@gmail.com>
2025-05-02 18:05:11 -0700

    Phase 2 done, added two routes, and updates readme for phase 2

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ P2P Network Project for CECS 327 – Intro to Networks and Distributed Computing
 Run:
 docker-compose up --build
 
-Mount volume in Docker (optional standalone test):
+Mount volume in Docker:
 docker run -d -p 5001:5000 -v "%cd%/storage:/app/storage" --name node1 p2p-node
 # Use $(pwd) instead of %cd% if on WSL or Git Bash
 
@@ -22,3 +22,16 @@ Expected response:
 Verify upload:
 curl.exe http://localhost:5001/download/mydoc.txt
 # Or open in browser: http://localhost:5001/download/mydoc.txt
+
+
+### Phase 2: Key-Value Store
+
+Test storing a key-value pair:
+curl.exe -X POST http://localhost:5001/kv -H "Content-Type: application/json" -d '{\"key\": \"color\", \"value\": \"blue\"}'
+
+Expected response: {"key":"color","status":"stored","value":"blue"}
+
+curl http://localhost:5001/kv/color
+# Or open in browser: curl http://localhost:5001/kv/color
+
+Expected response: {"key":"color","value":"blue"}
--- node.py
+++ node.py
@@ -14,6 +14,8 @@ my_address = os.environ.get("MY_ADDRESS", "http://localhost:5000")
 
 os.makedirs("storage", exist_ok=True)
 
+kv_store = {}
+
 @app.route("/")
 def index():
     return jsonify({"message": f"Node {node_id} is running!"})
@@ -39,6 +41,7 @@ def message():
     print(f"Received message from {sender}: {msg}", flush=True)
     return jsonify({"status": "received"})
 
+# Project 3:
 @app.route('/upload', methods=['POST'])
 def upload_file():
     file = request.files['file']
@@ -49,6 +52,24 @@ def upload_file():
 @app.route('/download/<filename>', methods=['GET'])
 def download_file(filename):
     return send_from_directory('./storage', filename)
+
+@app.route("/kv", methods=["POST"])
+def insert_kv():
+    data = request.get_json()
+    key = data.get("key")
+    value = data.get("value")
+    if key and value:
+        kv_store[key] = value
+        return jsonify({"status": "stored", "key": key, "value": value})
+    return jsonify({"error": "Missing key or value"}), 400
+
+@app.route("/kv/<key>", methods=["GET"])
+def get_kv(key):
+    value = kv_store.get(key)
+    if value is not None:
+        return jsonify({"key": key, "value": value})
+    return jsonify({"error": "Key not found"}), 404
+
 
 def register_with_bootstrap():
     try:
